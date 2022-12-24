f39d0fcdbecfe08f8a32a3a70eb4867c3d2dabff
robonexx <robertwagar@gmail.com>
2022-12-24 02:18:37 +0100

    .

--- animation.js
+++ animation.js
@@ -1,18 +1,20 @@
 let arr = Array.from(document.querySelectorAll('.textSplit'));
 
-console.log(arr);
-
+console.log('array of texts: ' + arr);
+/* 
 let winHeight = window.innerHeight;
 
-// Recalc height of window in case of resize
 window.addEventListener('resize', function () {
   winHeight = window.innerHeight;
-});
+}); */
 
 const observer = new IntersectionObserver(
-  (entries) => {
+  (entries, cls) => {
     entries.forEach((entry) => {
-      entry.target.classList.toggle('animated', entry.isIntersecting);
+      entry.target.classList.toggle('slideInLeft', entry.isIntersecting);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
       console.log(
         'this i classList from entry inside observer: ' + entry.target.classList
       );
@@ -20,39 +22,41 @@ const observer = new IntersectionObserver(
   },
   {
     threshold: 1,
+    rootMargin: '50px',
   }
 );
-/* 
+
 arr.forEach((a) => {
   let textLineArr = a.textContent.split(' ');
 
   console.log(textLineArr);
-  
+
   let html = '';
-  
+
   let animation = a.classList;
-  console.log(animation);
-  
+  console.log('animation class list: ' + animation);
+
   for (i = 0; i < textLineArr.length; ++i) {
-   
-    html.innerHTML +=
-      " <div style='animation-delay:" +
+    html +=
+      " <span style='animation-delay:" +
       i * 200 +
       "ms; display:inline-block' class='" +
       animation +
       "'>" +
       textLineArr[i] +
-      '</div>';
+      '</span>';
 
     console.log(html);
 
-    a.append(html);
+    a.innerHTML = html;
+
+    a.parentElement.classList.remove();
   }
 
   console.log(a.textContent);
-}); */
+});
 
-arr.forEach((arr) => {
-  console.log(arr.classList);
-  observer.observe(arr);
+arr.forEach((a) => {
+  console.log(a.classList);
+  observer.observe(a);
 });
